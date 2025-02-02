3636dda216cc0f93292b77bd58f68fc1fb698007
tomas-ryan-lee <contact@creative-eye.fr>
2025-02-02 17:29:26 +0100

    CRUD Image files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ImageController } from './image/image.controller';
+import { ImageService } from './image/image.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, ImageController],
+  providers: [AppService, ImageService],
 })
 export class AppModule {}
