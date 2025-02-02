a801cc48c1b3af3e2c8a9e0b60c86cc3e5ab0a34
tomas-ryan-lee <contact@creative-eye.fr>
2025-02-02 17:41:02 +0100

    Config upload

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ImageController } from './image/image.controller';
 import { ImageService } from './image/image.service';
+import { ImageModule } from './image/image.module';
 
 @Module({
-  imports: [],
+  imports: [ImageModule],
   controllers: [AppController, ImageController],
   providers: [AppService, ImageService],
 })
--- src/image/image.controller.ts
+++ src/image/image.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { ImageService } from './image.service';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('image')
-export class ImageController {}
+export class ImageController {
+  constructor(private readonly imageService: ImageService) {}
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadImage(@UploadedFile() file: Express.Multer.File) {
+    return this.imageService.optimizeImage(file);
+  }
+}
